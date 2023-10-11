6fe2ff6a5427a83f07794cb2d53c3ecf21b849b6
Ubuntu <ubuntu@ip-172-31-17-55.ec2.internal>
2023-10-11 13:18:30 +0000

    melhorias

--- app.py
+++ app.py
@@ -9,26 +9,15 @@ import requests
 app = Flask(__name__)
 
 def aws_get_metadata():
-    metadata = {
-        'public-hostname' : "",
-        'ami-id' : "",
-        'instance-id' : "",
-        'instance-type' : "",
-        'local-ipv4' : "",
-        'security-groups' : "",
-        'availability-zone' : "",
+    URL_AZ = 'http://169.254.169.254/latest/meta-data/placement/availability-zone'
+    URL_HOSTNAME = 'http://169.254.169.254/latest/meta-data/hostname'
+    resp_az = requests.get(URL_AZ)
+    resp_hostname = requests.get(URL_HOSTNAME)
+    meta_fields = {
+        "AZ": resp_az.text.split('\n'),
+        "hostname": resp_hostname.text.split('\n')
     }
-    for key in metadata:
-        resp = requests.get(
-            f'http://169.254.169.254/latest/meta-data/{key}',
-            timeout=2
-        )
-        if resp.status_code == 200:
-            raise Exception()
-        
-    return resp.json()
-
-    
+    return meta_fields
 
 def cpu_load(duration):
     end_time = time.time() + duration
@@ -45,18 +34,16 @@ def show_cpu():
 @app.route('/', methods=['GET', 'POST'])
 def form():
     
-    hostname = socket.gethostname()
+    info = aws_get_metadata()
     if request.method == 'POST':
        
         duration = int(request.form['duration'])
         for _ in range(0, multiprocessing.cpu_count()):
             process = multiprocessing.Process(target=cpu_load, args=(duration,))
             process.start()
-        
-        return(render_template('index.html',duration=duration, hostname=hostname))    
-           
-            
-    return render_template('form.html', hostname=hostname, dados=aws_get_metadata())
+        return(render_template('form.html',duration=duration, hostname=info["hostname"], AZ=info["AZ"]))    
+    
+    return render_template('form.html', hostname=info["hostname"], AZ=info["AZ"])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
