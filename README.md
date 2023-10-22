3114fd901618e364722a1481b875f697ed6540ba
Ubuntu <ubuntu@ip-172-31-21-189.ec2.internal>
2023-10-22 13:37:25 +0000

    separando a aplicação de teste em outro repo

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 # aws-cpuload
 App de teste para ELB e Auto Scaling Groups
 
-* "/books" apresenta uma lista de livros em um banco mysql
-* "/books/create" acresenta um livro no banco
+Exemplo para rodar: 
 
-É preciso ter um schema já criado e um usuário com poderes para criar as tabelas 
-
-É necessário passar uma variável de ambiente para o container com a string de conexão por exemplo 
-```bash
-
-docker run -p 8080:5000 -d -e DBSTRING='mysql://<usuario>:<senha>@<endpoint>:<porta>/<banco>' cquinta/aws-cpuload:main  
+```
+docker run -p 80:5000 -d cquinta/aws-cpuload:main  
 
 ```
 
--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
 import time
 import psutil
 import multiprocessing
@@ -10,26 +8,6 @@ import requests
 
 app = Flask(__name__)
 
-dbstring = os.environ["DBSTRING"]
-
-app.config['SQLALCHEMY_DATABASE_URI'] = dbstring
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-
-class Booklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    author = db.Column(db.String(200),nullable=False) 
-    resenha = db.Column(db.Text)
-    def __repr__(self):
-        return f'<Booklist {self.name}>'
-
-
-with app.app_context():
-    db.create_all()
-    
 
 
 def aws_get_metadata():
@@ -49,26 +27,7 @@ def cpu_load(duration):
         #render_template('index.html')
         pass
 
-@app.route('/books')
-def index():
-    books = db.session.execute(db.select(Booklist).order_by(Booklist.name)).scalars()
-    return render_template("booklist.html", books=books)
 
-
-@app.route('/books/create', methods=['GET', 'POST'])
-def books_create():
-    if request.method =="POST":
-        book=Booklist(
-            name=request.form['name'], 
-            author=request.form['author'], 
-            resenha=request.form['resenha'],)
-        db.session.add(book)
-        db.session.commit()
-        return render_template("book.html",book=book)
-    return render_template("bookcreate.html")
-    
-    
-    
 @app.route('/show_cpu')
 def show_cpu():
     cpu = psutil.cpu_percent(4)
